01abb83dd7084aaca967c3f4f75ffe0d9c9bd304
Polyane Tuag <pollyportugues@hotmail.com>
2022-06-09 13:37:05 -0300

    feat: card component

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+export function Card() {
+    return (
+        <div className='card'>
+            <strong>Carlos Silva</strong>
+            <small>10:30:10</small>
+        </div>
+    )
+}
